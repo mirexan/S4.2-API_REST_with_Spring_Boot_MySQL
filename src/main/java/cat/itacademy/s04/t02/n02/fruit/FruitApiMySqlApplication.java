0dfe7c0101c7c7000d6fe3663c399b75ba09a470
mirexan <mmregada@gmail.com>
2025-12-10 12:55:04 +0100

    chore:inital commit

---
+++ src/main/java/cat/itacademy/s04/t02/n02/fruit/FruitApiMySqlApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n02.fruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitApiMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitApiMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/s04/t02/n02/fruit/FruitApiMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n02.fruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitApiMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
